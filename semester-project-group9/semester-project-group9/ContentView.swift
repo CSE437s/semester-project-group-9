a354a94ad0233528439fa1ddf4a23758e6d8fff5
colesherman <89808563+colesherman@users.noreply.github.com>
2024-02-06 18:57:40 -0600

    Initial project

---
+++ semester-project-group9/semester-project-group9/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  semester-project-group9
+//
+//  Created by Cole Sherman on 2/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ semester-project-group9/semester-project-group9/semester_project_group9App.swift
@@ -0,0 +1,30 @@
+//
+//  semester_project_group9App.swift
+//  semester-project-group9
+//
+//  Created by Cole Sherman on 2/6/24.
+//
+
+import SwiftUI
+import FirebaseCore
+import FirebaseAuth
+import Firebase
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    print("Application is starting up. ApplicationDelegate didFinishLaunchingWithOptions.")
+    FirebaseApp.configure()
+    return true
+  }
+}
+
+@main
+struct semester_project_group9App: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

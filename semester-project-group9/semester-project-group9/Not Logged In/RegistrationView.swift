26cee16915628adc70b763c1556bc93f42d29a3a
aschwartz91 <77259180+aschwartz91@users.noreply.github.com>
2024-03-26 15:34:26 -0500

    registration View updated

--- semester-project-group9/semester-project-group9/Not Logged In/RegistrationView.swift
+++ semester-project-group9/semester-project-group9/Not Logged In/RegistrationView.swift
@@ -28,59 +28,120 @@ struct RegistrationView: View {
     
     var body: some View {
         
-        VStack {
+        VStack(alignment: .leading) {
             
             
-            Text("Register Account")
-                .padding()
-            
-            Spacer()
-            
-            TextField("First Name", text: $firstName)
-                .padding()
-            TextField("Last Name", text: $lastName)
-                .padding()
-            
-            TextField("Graduation Year", text: $graduationYear)
-                .padding()
-            
+            Group {
+                Text("Register Account")
+                    .bold()
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .padding()
+                    .font(.title)
+                
+                //Spacer()
+                
+                
+                Text("First Name:")
+                    .padding(.leading)
+                    .font(.caption)
+                
+                TextField("First Name", text: $firstName)
+                    .padding()
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 5) // Defines the shape of the outline
+                            .stroke(Color.gray, lineWidth: 1) // Sets the color and line width of the outline
+                    )
+                    .padding()
+                
+                
+                Text("Last Name:")
+                    .padding(.leading)
+                    .font(.caption)
+                TextField("Last Name", text: $lastName)
+                    .padding()
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 5) // Defines the shape of the outline
+                            .stroke(Color.gray, lineWidth: 1) // Sets the color and line width of the outline
+                    )
+                    .padding()
+                
+                
+                Text("Graduation year:")
+                    .padding(.leading)
+                    .font(.caption)
+                TextField("Graduation Year", text: $graduationYear)
+                    .padding()
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 5) // Defines the shape of the outline
+                            .stroke(Color.gray, lineWidth: 1) // Sets the color and line width of the outline
+                    )
+                    .padding()
+            }
+            Text("Major:")
+                .padding(.leading)
+                .font(.caption)
             TextField("Major", text: $firstMajor)
                 .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 5) // Defines the shape of the outline
+                        .stroke(Color.gray, lineWidth: 1) // Sets the color and line width of the outline
+                )
+                .padding()
             
+            Text("Second Major (if applicable):")
+                .padding(.leading)
+                .font(.caption)
             TextField("Second Major (if applicable)", text: $secondMajor)
+                .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 5) // Defines the shape of the outline
+                        .stroke(Color.gray, lineWidth: 1) // Sets the color and line width of the outline
+                )
                 .padding()
             
             
-            Spacer()
             
-            Button {
-                if validInput {
-                    // create user profile data and upload to database
-                    storeUserInformation()
-//                    presentImport = true
+            
+            VStack {
+                Spacer()
+                
+                Button {
+                    if validInput {
+                        // create user profile data and upload to database
+                        storeUserInformation()
+                        //                    presentImport = true
+                    }
+                } label : {
+                    Text("Proceed to Class Import")
                 }
-            } label : {
-                Text("Proceed to Class Import")
+                .navigationDestination(isPresented: $presentImport) {
+                    // tutorial/import view
+                    TutorialView()
+                    
+                }
+                .padding()
+                .frame(maxWidth: UIScreen.main.bounds.width * 0.6)
+                .background(Color.accentColor)
+                .cornerRadius(12)
+                .foregroundColor(.white)
+                Spacer()
+                
+                
+                // TODO: Input validation (if all inputs are valid [no fields empty, graduation year is somewhere between 2024-2030], set the validInput to be true)
+                
+                
+                
+                    .toolbar(.hidden)
+                    .alert("Invalid Input", isPresented: $showingAlert) {
+                        Button("OK", role: .cancel) { }
+                    }
+                message: {
+                    Text(alertMessage)
+                }
+                
             }
-            .navigationDestination(isPresented: $presentImport) {
-                // tutorial/import view
-                TutorialView()
-            }
-            .padding()
-            .background(Color.accentColor)
-            .cornerRadius(12)
-            .foregroundColor(.white)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
             
-            // TODO: Input validation (if all inputs are valid [no fields empty, graduation year is somewhere between 2024-2030], set the validInput to be true)
-            
-            
-        }
-        .toolbar(.hidden)
-        .alert("Invalid Input", isPresented: $showingAlert) {
-            Button("OK", role: .cancel) { }
-        }
-        message: {
-            Text(alertMessage)
         }
     }
     

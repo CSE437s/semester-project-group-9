47c1fedf5ff425a29c6993933210b5ad4944a0f7
colesherman <89808563+colesherman@users.noreply.github.com>
2024-04-02 16:57:38 -0500

    More fixes

--- semester-project-group9/semester-project-group9/FirebaseManager.swift
+++
@@ -1,9 +0,0 @@
-//
-//  FirebaseManager.swift
-//  semester-project-group9
-//
-//  Created by Cole Sherman on 2/25/24.
-//
-
-import Foundation
-import Firebase
---
+++ semester-project-group9/semester-project-group9/Not Logged In/Import Classes/ConfirmClassesView.swift
@@ -0,0 +1,18 @@
+//
+//  ConfirmClassesView.swift
+//  semester-project-group9
+//
+//  Created by Cole Sherman on 4/2/24.
+//
+
+import SwiftUI
+
+struct ConfirmClassesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ConfirmClassesView()
+}
